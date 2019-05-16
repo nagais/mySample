0ddb2eba680db402b201955c816dc908318ac1bb
nagais <vioret.hawk@gmail.com>
2019-05-16 22:14:13 +0900

    fix : いい感じに動くようにした

--- app/src/main/java/com/example/mysample/presentation/top/TopActivity.kt
+++ app/src/main/java/com/example/mysample/presentation/top/TopActivity.kt
@@ -1,11 +1,15 @@
 package com.example.mysample.presentation.top
 
+import android.animation.AnimatorInflater
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.mysample.R
+import com.example.mysample.databinding.ActivityTopBinding
 import com.example.mysample.presentation.util.BaseActivity
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_top.view.*
 
 
 class TopActivity : BaseActivity() {
@@ -20,11 +24,20 @@ class TopActivity : BaseActivity() {
         }
     }
 
+    private val binding: ActivityTopBinding by lazy {
+        DataBindingUtil.setContentView<ActivityTopBinding>(this, R.layout.activity_top)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         setNowActivity(this)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top)
+
+
+        val animatorSet = AnimatorInflater.loadAnimator(this, R.animator.repeat)
+        animatorSet.setTarget(binding.root.speaker)
+        animatorSet.start()
     }
 }
